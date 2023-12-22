1745244f3f28a9fcea36d678ca46abaeb22ae919
AndranikAroyan <154603969+AndranikAroyan@users.noreply.github.com>
2023-12-22 17:26:21 +0400

    Add files via upload

---
+++ Public/Src/components/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+import Container1 from './Container1';
+import Container2 from './Container2';
+import ContainerHard from './ContainerHard';
+import Container3 from './Container3';
+import Container5 from './Container5';
+import Container6 from './Container6';
+import LastOne from './LastOne';
+
+function App() {
+  return (
+    <div>
+      <Container1 />
+      <Container2 />
+      <ContainerHard />
+      <Container3 />
+      <Container5 />
+      <Container6 />
+      <LastOne />
+    </div>
+  );
+}
+export default App;
